1012c2560193a20c39f5a869bfba3ac07da34ec4
Julie Jonak <juliejonak10@gmail.com>
2019-07-27 14:46:42 -0500

    Scroll rendering added

--- scroll.js
+++ scroll.js
@@ -7,37 +7,122 @@
 
 
 const map = {
-    // Creates an 8x8 map
-    columns: 8,
-    rows: 8,
+    // Creates a 12x12 map
+    columns: 12,
+    rows: 12,
     // With tiles 64px x 64px in size
     tile_size: 64,
-    // sets up the tile designations for each spot on the board
-    tiles: [
-        1, 3, 3, 3, 1, 1, 3, 1,
-        1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 2, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 2, 1, 1, 1, 1,
-        1, 1, 1, 1, 2, 1, 1, 1,
-        1, 1, 1, 1, 2, 1, 1, 1,
-        1, 1, 1, 0, 0, 1, 1, 1
-    ]
+    // sets up the tile designations for each spot on the board in the first array
+    // Adds the layer tiles in the second array
+    layers: [[
+        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3,
+        3, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3,
+        3, 3, 3, 1, 1, 2, 3, 3, 3, 3, 3, 3
+    ], [
+        4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 5, 0, 0, 0, 0, 0, 5, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 4, 4, 0, 5, 4, 4, 4, 4, 4, 4, 4,
+        4, 4, 4, 0, 0, 3, 3, 3, 3, 3, 3, 3
+    ]]
 }
 
-// Fetches the appropriate tile for a specific spot
-map.getTile = (column, row) => {
-    return this.tiles[row * map.columns + column]
+// Fetches the appropriate tile for a specific spot, plus any layering
+map.getTile = (layer, column, row) => {
+    return this.layers[layer][row * map.columns + column]
 };
+
+// Checks if a tile is solid (an obstacle)
+// Tiles 3 and 5 are solid. The rest can be walked on
+// Function looks through all layers at the coord and returns TRUE if one is solid
+map.isSolidTileAtXY = (x,y) => {
+    const column = Math.floor(x / this.tile_size);
+    const row = Math.floor(y / this.tile_size);
+
+    return this.layers.reduce((res, layer, index) => {
+        const tile = this.getTile(index, column, row);
+        const isSolid = tile === 3 || tile === 5;
+
+        // If True, will return true; else default of false
+        return res || isSolid
+    }, false) // Passes in false as default res value
+}
 
 // Iterates over the columns and rows to render the map:
 // context: a 2D canvas context
 // titleAtlas: an image object that contains the tile map
 // map: the tile map object
 
+
+// NON-SCROLL MAP RENDERING
+
+// for (let i=0; i < map.columns; i++){
+//     for (let x=0; x < map.rows; x++){
+//         const tile = map.getTile(i,x);
+//         // If NOT an empty tile (0)
+//         if (tile !== 0){
+//             context.drawImage( 
+//                 // the tileset image
+//                 tileAtlas,
+//                 // top left x-axis coord of source
+//                 (tile - 1) * map.tile_size, 
+//                 // top left y-axis coord of source
+//                 0,
+//                 // width to draw from source (64px)
+//                 map.tile_size,
+//                 // height to draw from source (64px)
+//                 map.tile_size,
+//                 // x-axis coord to place top left of new image
+//                 i * map.tile_size,
+//                 // y-axis coord to place top left of new image
+//                 x * map.tile_size,
+//                 // the width to draw in the canvas (64px)
+//                 map.tile_size,
+//                 // the height to draw in the canvas (64px)
+//                 map.tile_size
+//             )
+//         }
+//     }
+// }
+
+// SCROLL-VIEW RENDERING
+
+// To scroll view, we need to partially render some tiles
+// This requires tracking our starting points
+
+const startColumn = Math.floor(this.camera.x / map.tile_size);
+const endColumn = startColumn + (this.camera.width / map.tile_size);
+const startRow = Math.floor(this.camera.y / map.tile_size);
+const endRow = startRow + (this.camera.height / map.tile_size);
+
+// We'll calculate how much to offset the tiles from the typical starting (0,0) point based on keeping the sprite centered in the camera view
+
+const offsetX = -this.camera.x + startColumn * map.tile_size;
+const offsetY = -this.camera.y + startRow * map.tile_size;
+
+// The loop is similar except adds the offset values to x and y coords
+// Plus we have to round those values to avoid floating points
 for (let i=0; i < map.columns; i++){
-    for (let x=0; x < map.rows; x++){
-        const tile = map.getTile(i,x);
+    for (let j=0; j < map.rows; j++){
+        const tile = map.getTile(i,j);
+        const X = (i - startColumn) * map.tile_size + offsetX;
+        const Y = (i - startRow) * map.tile_size + offsetY;
         // If NOT an empty tile (0)
         if (tile !== 0){
             context.drawImage( 
@@ -52,9 +137,9 @@ for (let i=0; i < map.columns; i++){
                 // height to draw from source (64px)
                 map.tile_size,
                 // x-axis coord to place top left of new image
-                i * map.tile_size,
+                Math.round(X),
                 // y-axis coord to place top left of new image
-                x * map.tile_size,
+                Math.round(y),
                 // the width to draw in the canvas (64px)
                 map.tile_size,
                 // the height to draw in the canvas (64px)
@@ -62,4 +147,5 @@ for (let i=0; i < map.columns; i++){
             )
         }
     }
-}
+};
+
